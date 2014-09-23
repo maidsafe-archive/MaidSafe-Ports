ad58eeab21032040a536b18522fcf52eacea6080
Spandan <spandana@karyatech.com>
2014-09-23 19:53:35 +0530

    fix for auto generation of lib and include paths in binding.gyp

--- nodejs/sample.js
+++ nodejs/sample.js
@@ -4,11 +4,13 @@ var testClassObject = nodejsPort.TestClass.RawFactoryFunction();
 
 var randomInt = Math.round((Math.random() * (1000 - 1)) + 1);
 
-console.log('Random Int: ' + randomInt);
+console.log('Random Int:', randomInt);
 
 console.log('Passing var to C++');
 testClassObject.SetMyValue(randomInt);
 
-console.log('Getting var from C++: ' + testClassObject.GetMyValue());
-console.log('Getting Static var from C++: ' + testClassObject.GetStaticConstValue());
+console.log('Getting var from C++:', testClassObject.GetMyValue());
+
+console.log('Getting Random UInt from MS-Common>utils.h:', testClassObject.GetRandomUInt32())
+
 console.log('\nDone...');
